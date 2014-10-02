eef151a9ecf6f68e371324f2ffa57a426b4db9f8
Jonathon Walker <jonathonwalker93@gmail.com>
2014-10-02 22:30:09 +0200

    utils file complete

---
+++ assignment2_utils.py
@@ -0,0 +1,54 @@
+import random
+import os
+import sys
+
+def test(sorting_algorithm):
+	# check if nums.txt exists
+	if not os.path.exists('nums.txt'):
+		print("First create nums.txt")
+		sys.exit(0)
+
+	# read the content of nums.txt into an array
+	nums = open('nums.txt', 'r')
+	a = []
+	for line in nums:
+		a.append(int(str.strip(line)))
+
+	# sort the array using bubblesort
+	# You need to change here to call your sorting algorithm
+	sorting_algorithm(a)
+
+	# output nums_sorted.txt
+	nums_sorted = open('nums_sorted.txt', 'w')
+	for element in a:
+		nums_sorted.write(str(element) + "\n")
+
+	nums.close()
+	nums_sorted.close()
+
+	# compare your result (nums_sorted.txt) against the result of bubblesorting algorithm (nums_ref.txt)
+	# os.system('sort nums.txt < nums_ref.txt')
+	if not os.name == 'posix':
+		nums_ref = open('nums_ref.txt', 'w')
+		for element in sorted(a):
+			nums_ref.write(str(element) + "\n")
+		nums_ref.close()
+		ret = os.system('FC nums_sorted.txt nums_ref.txt')
+	else:
+		os.system('sort -n nums.txt > nums_ref.txt')
+		ret = os.system('diff nums_sorted.txt nums_ref.txt')
+
+	# output result
+	if int(ret) == 0:
+		print("Sorted!")
+
+	if int(ret) == 1:
+		print("Not sorted!")
+
+
+
+
+
+
+
+
--- insertion_sort.py
+++ insertion_sort.py
@@ -1,11 +1,12 @@
-A = [1, 3, 2, 5, 7, 8, 4, 6]
+import assignment2_utils
 
-for j in range(1, len(A)):
-	key = A[j]
-	i = j - 1
-	while i > 0 and A[i] > key:
-		A[i + 1] = A[i]
-		i -= 1
-	A[i + 1] = key 
+def insertion_sort(a):
+	for i in range(1, len(a)):
+		key = a[i]
+		j = i - 1
+		while j >= 0 and a[j] > key:
+			a[j + 1] = a[j]
+			j -= 1
+		a[j + 1] = key
 
-print(A)
+assignment2_utils.test(insertion_sort)
